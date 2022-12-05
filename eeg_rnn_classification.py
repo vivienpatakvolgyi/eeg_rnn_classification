b5c78a482ab6a7795182153e6a2e978877e029e0
vivienpatakvolgyi <92115840+vivienpatakvolgyi@users.noreply.github.com>
2022-12-05 22:13:09 +0100

    a test streamlit file was uploaded

---
+++ eeg_rnn_classification.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('RNN classification with EEG data')
+uploaded_file = st.file_uploader("Upload CSV", type=".csv")
+st.write(uploaded_file)
