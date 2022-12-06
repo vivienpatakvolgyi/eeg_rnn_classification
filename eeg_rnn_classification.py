dfe7c4c86da41a7a0554f76debae145f4d550c16
vivienpatakvolgyi <92115840+vivienpatakvolgyi@users.noreply.github.com>
2022-12-06 14:27:39 +0100

    vscode browser test

--- eeg_rnn_classification.py
+++ eeg_rnn_classification.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('RNN classification with EEG data')
+st.title('RNN classification with EEG DATA')
 uploaded_file = st.file_uploader("Upload CSV", type=".csv")
 st.write(uploaded_file)
