32bed18c91827bd5f62bef7a2470cb261684960e
vivienpatakvolgyi <92115840+vivienpatakvolgyi@users.noreply.github.com>
2022-12-06 14:35:59 +0100

    train mode selector added

--- eeg_rnn_classification.py
+++ eeg_rnn_classification.py
@@ -2,6 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('RNN classification with EEG DATA')
+st.title('RNN classification with EEG data')
+
+train_test = st.radio(
+    "I used two different approaches, each has different outcome, so please select which one you want to use for prediction:",
+    ('a) We train the model on the data of three of the four users and predict on the remaining 1', 'b) The data used for prediction are randomly selected from the time series data'))
+
+st.write('Each prediction has ')
 uploaded_file = st.file_uploader("Upload CSV", type=".csv")
 st.write(uploaded_file)
