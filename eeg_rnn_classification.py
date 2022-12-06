326a91d9a21371db924a2802832682759323bda1
vivienpatakvolgyi <92115840+vivienpatakvolgyi@users.noreply.github.com>
2022-12-06 16:44:26 +0100

    texts for the two options are uploaded

--- eeg_rnn_classification.py
+++ eeg_rnn_classification.py
@@ -15,9 +15,12 @@ if results:
 
     if 'a)' in train_test:
         st.write('You chosed the A option. [Open original notebook](https://colab.research.google.com/drive/1wMTgQV1En_W1eQONhEyipMb2i9EmIbZ4?usp=sharing)')
+        st.write('In this case, you can upload the fourth user\'s data in csv. After downloading the original .csv file, please drag and drop here to start the prediction and see the results')
     elif 'b)' in train_test:
         st.write('You chosed the B option. [Open original notebook](https://colab.research.google.com/drive/1JWahgKnkjCOrddkxIRy8vpQqH4QXnSkY?usp=sharing)')
+        st.write('Sadly, in this case we don\'t have the data in csv, because it was separated during the data processing. However, I\'m gonna show you the processed data structure right before we use it for prediction. In option a), after data processing, we have the same exact structure but with different data. This data is only the 20\% of the full dataset and it was selected randomly from all user\'s time series data as sequences. In option a), we used 25% of the full dataset for prediction and it\'s from an entirely different user than the ones we used for training.')
         
+        st.write('As you can see, the rate of correctly predicted values ​​is higher in this case, while in the first case we get a value of around 30%, which means that the prediction efficiency is the same as random guessing (considering that we have three categories).')
 
 uploaded_file = st.file_uploader("Upload CSV", type=".csv")
 st.write(uploaded_file)
