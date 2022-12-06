005b6d9a5d1fef9b77c55e16932e54e8474c273e
vivienpatakvolgyi <92115840+vivienpatakvolgyi@users.noreply.github.com>
2022-12-06 14:38:11 +0100

    executing after selection

--- eeg_rnn_classification.py
+++ eeg_rnn_classification.py
@@ -8,6 +8,14 @@ train_test = st.radio(
     "I used two different approaches, each has different outcome, so please select which one you want to use for prediction:",
     ('a) We train the model on the data of three of the four users and predict on the remaining 1', 'b) The data used for prediction are randomly selected from the time series data'))
 
-st.write('Each prediction has ')
+results = st.button('Show results', key='res')
+
+if results: 
+    if train_test == 0:
+        st.write('You chosed the A option')
+    elif train_test == 1:
+        st.write('You chosed the B option')
+        
+
 uploaded_file = st.file_uploader("Upload CSV", type=".csv")
 st.write(uploaded_file)
