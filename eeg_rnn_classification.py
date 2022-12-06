7cd49a1d115e391d113a7231189fc69ed1639951
vivienpatakvolgyi <92115840+vivienpatakvolgyi@users.noreply.github.com>
2022-12-06 21:09:18 +0100

    handling csv

--- eeg_rnn_classification.py
+++ eeg_rnn_classification.py
@@ -30,7 +30,7 @@ if 'a)' in train_test:
         st.download_button('Download csv file', f, file_name='user_d.csv')
     uploaded_file = st.file_uploader("Upload csv file", type=".csv")
     if uploaded_file:
-        item = pd.read_csv(uploaded_file)
+        item = uploaded_file.getvalue()
         scaler = StandardScaler()
         cols = df[['Class', 'UserId']]
         df = pd.DataFrame(scaler.fit_transform(df.drop(['Class', 'UserId'], axis = 1)),columns=df.columns.drop(['Class', 'UserId']))
