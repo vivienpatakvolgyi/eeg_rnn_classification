a99873cf2db029ab981ed1cf49ef8956e24e315e
vivienpatakvolgyi <92115840+vivienpatakvolgyi@users.noreply.github.com>
2022-12-06 14:39:40 +0100

    executing after selection

--- eeg_rnn_classification.py
+++ eeg_rnn_classification.py
@@ -4,13 +4,14 @@ import numpy as np
 
 st.title('RNN classification with EEG data')
 
-train_test = st.radio(
+train_test = int(st.radio(
     "I used two different approaches, each has different outcome, so please select which one you want to use for prediction:",
-    ('a) We train the model on the data of three of the four users and predict on the remaining 1', 'b) The data used for prediction are randomly selected from the time series data'))
+    ('a) We train the model on the data of three of the four users and predict on the remaining 1', 'b) The data used for prediction are randomly selected from the time series data')))
 
 results = st.button('Show results', key='res')
 
 if results: 
+    st.write(train_test)
     if train_test == 0:
         st.write('You chosed the A option')
     elif train_test == 1:
