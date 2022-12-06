3713dc2e1d17d68ba4aed8262137e2f117281abc
vivienpatakvolgyi <92115840+vivienpatakvolgyi@users.noreply.github.com>
2022-12-06 21:25:51 +0100

    a) predict

--- eeg_rnn_classification.py
+++ eeg_rnn_classification.py
@@ -47,11 +47,10 @@ if 'a)' in train_test:
     if uploaded_file:
         item = pd.DataFrame(pd.read_csv(uploaded_file))
         scaler = StandardScaler()
-        st.write(item['Class'])
         cols = item['Class']
         df = pd.DataFrame(scaler.fit_transform(item.drop(['Class'], axis = 1)),columns=item.columns.drop(['Class']))
         st.write(df)
-        df['Class'] =cols['Class']
+        df['Class'] =cols
         X_val, y_val = append_time_series(df)
         generator_2 = TimeseriesGenerator(X_val, y_val, length=15, batch_size=32, shuffle = True)
 
